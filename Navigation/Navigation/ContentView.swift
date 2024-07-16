e693c505283ee9b9adc1c79d0b6ae8142d427a48
galih <samudragalih94@gmail.com>
2024-07-16 10:07:33 +0700

    feat(UI): create simple UI to push navigation

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,15 +7,26 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+        Text("Detail view: \(number)")
+    }
+    
+    init(number: Int) {
+        self.number = number
+        print("creating detail view: \(number)")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            NavigationLink("Tap me") {
+                DetailView(number: 102)
+            }
         }
-        .padding()
     }
 }
 
