cde48f10769a612c78e5d45219be73ffee18da4a
galih <samudragalih94@gmail.com>
2024-07-17 09:24:53 +0700

    clean code

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -31,20 +31,8 @@ struct DetailView: View {
 }
 
 struct ContentView: View {
-    let students: [Student] = [
-        Student(name: "Galih", number: 1),
-        Student(name: "Samodra", number: 2),
-        Student(name: "Wicaksono", number: 3)
-    ]
     var body: some View {
-        NavigationStack {
-            ForEach(students, id: \.id) { student in
-                NavigationLink("Select student \(student.name)", value: student)
-            }
-            .navigationDestination(for: Student.self) { selectonStudent in
-                DetailView(student: selectonStudent)
-            }
-        }
+        Text("hello world")
     }
 }
 
